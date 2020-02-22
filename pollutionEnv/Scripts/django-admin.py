d066426b5cac01f489f4918fbb772f96a6c6c40b
Shaheer <shaheerfarooqui2@gmail.com>
2020-02-23 02:39:31 +0530

    Pollution Index 1 Deploy

---
+++ pollutionEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Lenovo.DESKTOP-QQIMMN3\Desktop\pollutionIndex\pollutionEnv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
